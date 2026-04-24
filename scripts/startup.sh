19ddce29d26e11057cd077fb1d45a3d74b31a9cf
Aman <aman@kinetik.care>
2026-04-24 18:13:31 +0600

    fix

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Starting server..."
+echo "Booting up the server and GitHub Actions runner..."
+
 cd ~/actions-runner
 echo "Setting up GitHub Actions runner..."
 ./run.sh &
+
 echo "Running containers..."
+cd ~/docker-github-actions/scripts
 ./containers.sh &
