7d96b9028c1992c34d7a387c9fa9e593354666bf
Aman <aman@kinetik.care>
2026-04-24 19:23:37 +0600

    retest

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Booting up the server..."
+echo "Booting up the server and GitHub Actions runner..."
+
+cd ~/actions-runner
+echo "Setting up GitHub Actions runner..."
+./run.sh > output.log &
 
 echo "Running containers..."
 cd ~/docker-github-actions/scripts
