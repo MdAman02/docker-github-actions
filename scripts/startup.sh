1414019e0829f3eb65ab39d525dd9deb978d1582
Aman <aman@kinetik.care>
2026-04-24 19:37:22 +0600

    separate github runner service

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "Booting up the server and GitHub Actions runner..."
-
-cd ~/actions-runner
-echo "Setting up GitHub Actions runner..."
-./run.sh > output.log &
+echo "Booting up the server..."
 
 echo "Running containers..."
 cd ~/docker-github-actions/scripts
