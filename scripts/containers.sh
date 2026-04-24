1b4207d2721ff4fb6e1a45837dc290079de9c57c
Aman <aman@kinetik.care>
2026-04-24 20:55:52 +0600

    add docker login on startup

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -2,4 +2,5 @@
 echo "Cleaning up existing containers..."
 sudo docker rm -f $(sudo docker ps -aq)
 sudo docker image rm mdamanu/aman:latest
+cd ~/docker-github-actions
 sudo docker compose up -d
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Booting up the servers..."
+echo $DOCKERHUB_KEY | sudo docker login -u mdamanu --password-stdin
 echo "Running containers..."
 cd ~
 ./containers.sh
